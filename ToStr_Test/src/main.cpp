d2e2503add52ad333464c76850dd02569869edf0
underwatergrasshopper <underwater.grasshopper@gmail.com>
2023-01-15 13:07:56 +0100

    Added basic functionality.

--- ToStr_Test/src/main.cpp
+++ ToStr_Test/src/main.cpp
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <windows.h>
 
 #include "ToStr.h"
 
 int main() {
     puts(ToStr("", "").c_str());
+
+    enum { LEN = 16 };
+    char buffer[LEN];
+    memset(buffer, '.', LEN);
+
+    int length = snprintf(buffer, 8, "%s", "1234567");
+
+    printf("%d\n", length);
+    printf("%.*s\n", 16, buffer);
+
+
     return 0;
 }
--- include/ToStr.h
+++ include/ToStr.h
@@ -34,22 +34,97 @@
 #include <string>
 #include <utility>
 
-template <typename... Types>
-std::string ToStr(const char* format, Types&&... args);
+//------------------------------------------------------------------------------
+// Declarations
+//------------------------------------------------------------------------------
 
 template <typename... Types>
-std::string ToStr(const std::string& format, Types&&... args);
-
-//==============================================================================
+std::string ToStr(const char* format, Types&&... arguments);
 
 template <typename... Types>
-inline std::string ToStr(const char* format, Types&&... args) {
-    return "Place holder text.";
+std::string ToStr(const std::string& format, Types&&... arguments);
+
+void ToStr_SetHandleFatalErrorMessageFunction(void (*handle_fatal_error_message)(const char* message));
+void ToStr_DefaultHandleFatalErrorMessage(const char* message);
+
+//------------------------------------------------------------------------------
+// Inner
+//------------------------------------------------------------------------------
+
+struct ToStr_Data {
+    void (*handle_fatal_error_message)(const char* message);
+};
+
+inline ToStr_Data& ToStr_ToData() {
+    static ToStr_Data s_data = {
+        ToStr_DefaultHandleFatalErrorMessage
+    };
+
+    return s_data;
 }
 
 template <typename... Types>
-inline std::string ToStr(const std::string& format, Types&&... args) {
-    return ToStr(format.c_str(), std::forward<Types>(args)...);
+inline std::string ToStr(const char* format, Types&&... arguments) {
+
+    auto FatalError = [](const char* message) {
+        ToStr_Data& data = ToStr_ToData();
+        if (data.handle_fatal_error_message) data.handle_fatal_error_message(message);
+        exit(EXIT_FAILURE);
+    };
+
+    std::string text;
+
+    enum { SIZE = 4096 };
+    char buffer[SIZE];
+
+    const int length = snprintf(buffer, SIZE, format, arguments...);
+
+    if (errno != 0) {
+        if (errno == EINVAL) FatalError("ToStr Error: Wrong argument.");
+        FatalError("ToStr Error: Writing to buffer failed.");
+    } 
+    if (length < 0) {
+        FatalError("ToStr Error: Encoding error.");
+    }
+    if (length >= SIZE) {
+        const size_t    ext_size    = length + 1;
+        char*           ext_buffer  = new char[ext_size];
+
+        const int same_length = snprintf(ext_buffer, ext_size, format, arguments...);
+
+        if (errno != 0) {
+            if (errno == EINVAL) FatalError("ToStr Error: Wrong argument at second writing to buffer.");
+            FatalError("ToStr Error: Second writing to buffer failed.");
+        }
+        if (same_length < 0) {
+            FatalError("ToStr Error: Encoding error at second writing to buffer.");
+        }
+        if (same_length != length) {
+            FatalError("ToStr Error: Message actual length miss-match between first and second write to buffer.");
+        }
+
+        text = std::string(ext_buffer, same_length);
+
+        delete[] ext_buffer;
+    } else {
+        text = std::string(buffer, length);
+    }
+
+    return text;
+}
+
+template <typename... Types>
+inline std::string ToStr(const std::string& format, Types&&... arguments) {
+    return ToStr(format.c_str(), std::forward<Types>(arguments)...);
+}
+
+inline void ToStr_SetHandleFatalErrorMessageFunction(void (*handle_fatal_error_message)(const char* message)) {
+    ToStr_ToData().handle_fatal_error_message = handle_fatal_error_message;
+}
+
+inline void ToStr_DefaultHandleFatalErrorMessage(const char* message) {
+    puts(message);
+    fflush(stdout);
 }
 
 #endif // TOSTR_H_
